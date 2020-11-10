c0cb14f6935d6064f991ccbeb5feaff524c19dfa
Danielsear <danielandradeseara@hotmail.com>
2020-11-10 12:07:28 -0300

    create routes home GET and POST

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.json());
 
+require("../models/home");
+const Home = mongosse.model("Home");
+
 mongosse
   .connect("mongodb://localhost/celke", {
     useNewUrlParser: true,
@@ -17,8 +20,43 @@ mongosse
     console.log("Conexão com o mongodb deu erro: " + err);
   });
 
-app.get("/", (req, res) => {
-  res.json({ title: "Server ruining!" });
+app.get("/home", (req, res) => {
+  Home.findOne({})
+    .then((home) => {
+      return res.json({
+        error: false,
+        home,
+      });
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        error: true,
+        message: "Nenhum registro encontrado!",
+      });
+    });
+});
+
+app.post("/home", async (req, res) => {
+  const homeExite = await Home.findOne({});
+  if (homeExite) {
+    return res.status(400).json({
+      error: true,
+      message: "Erro: Conteudo da pagina Home ja tem um registro.",
+    });
+  }
+
+  Home.create(req.body, (err) => {
+    if (err) {
+      return res.status(400).json({
+        error: true,
+        message: "Erro: Conteudo da pagina Home não cadastrado com sucesso.",
+      });
+    }
+    return res.json({
+      error: false,
+      message: "Conteudo da pagina Home cadastrado com sucesso.",
+    });
+  });
 });
 
 app.listen(8080, () => {
