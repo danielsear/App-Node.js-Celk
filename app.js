e583be9662358c6123d852b5a81a9bacd7eb6ae1
Danielsear <danielandradeseara@hotmail.com>
2020-11-09 23:55:05 -0300

    conect mongodb

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const mongosse = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
+
+mongosse
+  .connect("mongodb://localhost/celke", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Conexão com o mongodb foi um sucesso!");
+  })
+  .catch((err) => {
+    console.log("Conexão com o mongodb deu erro: " + err);
+  });
 
 app.get("/", (req, res) => {
   res.json({ title: "Server ruining!" });
